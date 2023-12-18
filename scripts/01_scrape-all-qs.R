b39402b68e7871d8501e639fd0f3463557834878
Len Metson <lennardmetson@gmail.com>
2023-12-18 19:23:51 +0000

    add code to quarto and write tables to dataframe

--- scripts/01_scrape-all-qs.R
+++ scripts/01_scrape-all-qs.R
@@ -61,57 +61,9 @@ pull_all_oral_qs <- function(endpoint_url){
   return(response)
 }
 
-pull_all_written_qs <- function(endpoint_url){
-
-  # Calculate how many questions are in the end point
-  n_resp <- GET(paste0(endpoint_url, "?parameters.take=1")) |>
-    content("parsed")
-  n <- n_resp$totalResults
-
-
-  # Questions can be pulled in batches of 100,
-  # calculate how many time we will have to pull
-  n_loops <- ceiling(n / 100)
-
-  print(paste0("Written Qs started at ", Sys.time()))
-
-  for (i in 1:n_loops) {
-
-    n_skip <- (i - 1) * 100 # Skip however many 100s the loop has run
-
-    if (i == 1) { # On first iteration, make new list
-
-      response <- get_qs(endpoint_url, n_skip)
-      response <- response$results
-
-    } else { # On all other iterations, append to existing list
-
-      response_new <- get_qs(endpoint_url, n_skip)
-      response_new <- response_new$results
-      response <- c(response, response_new) # Merge responses
-
-    }
-
-    print(paste0(i, " of ", n_loops, " done.")) # Print progress message
-    Sys.sleep(1) # Sleep to avoid hammering the API 
-
-  }
-
-  print(paste0("Written Qs done at ", Sys.time()))
-  return(response)
-}
-
 ## APPLY FUNCTIONS
 
-# oral_questions <- pull_all_oral_qs(
-#   "https://oralquestionsandmotions-api.parliament.uk/oralquestions/list")
+oral_questions <- pull_all_oral_qs(
+  "https://oralquestionsandmotions-api.parliament.uk/oralquestions/list")
 
-# saveRDS(oral_questions, "data/oral_questions.RDS")
-
-written_questions <- pull_all_written_qs(
-  "https://questions-statements-api.parliament.uk/api/writtenquestions/questions")
-
-saveRDS(written_questions, "data/written_questions.RDS")
-
-
-
+saveRDS(oral_questions, "data/oral_questions.RDS")
