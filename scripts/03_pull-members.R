b41333c7d2b93d54ff33a46e85f27a68df10ded6
Len Metson <lennardmetson@gmail.com>
2023-12-21 15:27:13 +0000

    Test pull members script

--- scripts/03_pull-members.R
+++ scripts/03_pull-members.R
@@ -16,35 +16,58 @@ load_packages(c(
     ))
 
 
-progress_perc <- function(num_done, total, additional_message = ""){
-    cat("\r", paste0(additional_message, " ", round(num_done / total * 100, digits = 10), "% "))
+# Function defined by ChatGPT and adapted for the specific list structure here
+replace_null_with_na <- function(x) {
+  if (is.list(x)) { # Checks for whether the item is a sublist 
+    lapply(x, replace_null_with_na) # if it is, apply the function for each of the elements within the sublist
+  } else { # If it isn't, simply apply the main function
+    ifelse(is.null(x) || x == "null", "NA", x) 
+  }
 }
-
 
 
 pull_members <- function(base_url, df) {
 
+  cat("\n")
+  print(paste0("Started at ", Sys.time()))
+  cat("\n")
+
+  pb <- txtProgressBar(min = 0, max = nrow(df), style = 3)
+  
   for (i in seq_along(df$member_asking_Mnis_ID)) {
 
     url <- paste0(base_url, "/", df$member_asking_Mnis_ID[i], "?detailsForDate=", df$question_tabled_when[i]) 
 
     if (i == 1) {
-      response <- GET(url) %>% content("parsed")
-      response <- response[[1]]
+
+      response <- GET(url) %>%
+        content("parsed")
+      response <- response[1]
+      response <- c(date = df$question_tabled_when[i], response[[1]])
+
+      response <- list(response)
+
     } else {
       response_new <- GET(url) %>% content("parsed")
-      response_new <- response_new[[1]]
+      response_new <- response_new[1]
+      response_new <- c(date = df$question_tabled_when[i], response_new[[1]])
+
+      response_new <- list(response_new)
+
       response <- c(response, response_new) # Merge responses
     }
-    Sys.sleep(2)
 
-    progress_perc(i, length(df$member_asking_Mnis_ID), "Querying:")
+    Sys.sleep(0.5)
+
+    setTxtProgressBar(pb, i)
+
 
   }
 
-  print("Done.")
-
-  return(df)
+  cat("\n")
+  print(paste0("Done at ", Sys.time(), " :))"))
+  
+  return(response)
 }
 
 # Connect to database
@@ -56,6 +79,8 @@ questions <- dbGetQuery(db,
   "
   SELECT member_asking_Mnis_ID, question_tabled_when
   FROM oral_questions
+  ORDER BY RANDOM()
+  LIMIT 10
   "
 )
 
@@ -66,8 +91,48 @@ questions <- questions %>%
 
 members <- pull_members("https://members-api.parliament.uk/api/Members", questions)
 
+saveRDS(members, "data/members_raw.Rds")
 
-saveRDS(members, "data/members.Rds")
+members <-  lapply(members, function(lst) {lapply(lst, replace_null_with_na)})
+
+for (i in seq_along(members)) {
+
+  if (i == 1) {
+
+    members_df <- members[i] %>%
+      unlist() %>%
+      t() %>%
+      data.frame()
+
+  } else {
+    members_df_new <- members[i] %>%
+      unlist() %>%
+      t() %>%
+      data.frame()
+
+    members_df <- rbind(members_df, members_df_new)
+  }
+}
+
+glimpse(members_df)
+
+members_df <- members_df %>%
+  select(
+    member_date_valid = date, 
+    member_Mnis_ID = id, 
+    member_name_display = nameDisplayAs, 
+    member_latest_party = latestParty.id,
+    member_gender = gender, 
+    member_latest_constituency = latestHouseMembership.membershipFromId,
+    member_membership_start_date = latestHouseMembership.membershipStartDate, 
+    member_membership_end_date = latestHouseMembership.membershipEndDate,
+    member_membership_end_reason = latestHouseMembership.membershipEndReason,
+    member_name_full = nameFullTitle,
+    member_name_list = nameListAs
+  )
+
+
+dbWriteTable(db, "members", members_df, overwrite = TRUE)
 
 # Disconnect from local database
 DBI::dbDisconnect(db)
