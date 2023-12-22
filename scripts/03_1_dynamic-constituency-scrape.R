6512736bbfac5a972611cbbe0c3b88de915e8b42
Len Metson <lennardmetson@gmail.com>
2023-12-22 15:44:46 +0000

    Add try catch to selinum scraper

--- scripts/03_1_dynamic-constituency-scrape.R
+++ scripts/03_1_dynamic-constituency-scrape.R
@@ -63,7 +63,6 @@ selector_list$uc_claimants <- "/html/body/div[1]/report-embed/div/div[1]/div/div
 selector_list$house_prices <- "/html/body/div[1]/report-embed/div/div[1]/div/div/div/div/exploration-container/div/div/docking-container/div/div/div/div/exploration-host/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[39]/transform/div/div[3]/div/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[2]/div[1]"
 
 
-
 constituency_dash_scraper <- function(constituency_name){
 
   driver$navigate("https://commonslibrary.parliament.uk/constituency-dashboard/")
@@ -73,51 +72,83 @@ constituency_dash_scraper <- function(constituency_name){
   iframe <- driver$findElement(using = "xpath", value = "//iframe[@title='Constituency dashboard']")
   driver$switchToFrame(iframe)
 
-  Sys.sleep(2)
+  Sys.sleep(3)
   search_dropdown <- driver$findElement(using = "xpath", value = selector_list$search_dropdown)
   search_dropdown$clickElement()
 
-  Sys.sleep(2)
+  Sys.sleep(3)
   search_box <- driver$findElement(using = "xpath", value = selector_list$search_box)
   #search_box$clickElement()
   search_box$clearElement()
-  Sys.sleep(0.3)
+  Sys.sleep(1)
   search_box$sendKeysToElement(list(constituency_name))
 
-  Sys.sleep(1)
+  Sys.sleep(3)
   first_result <- driver$findElement(using = "xpath", value = selector_list$search_result)
   first_result$clickElement()
 
 
   # Extract text 
 
-  Sys.sleep(2)
+  Sys.sleep(4)
 
-  region_nation <- driver$findElement(using = "xpath", value = selector_list$region_nation)
-  region_nation_text <- region_nation$getElementText()[[1]]
+  tryCatch({
+    region_nation <- driver$findElement(using = "xpath", value = selector_list$region_nation)
+    region_nation_text <- region_nation$getElementText()[[1]]
+  }, error = function(e) {
+    region_nation_text <- NA
+  })
 
-  population <- driver$findElement(using = "xpath", value = selector_list$population)
-  population_text <- population$getElementText()[[1]]
+  tryCatch({
+    population <- driver$findElement(using = "xpath", value = selector_list$population)
+    population_text <- population$getElementText()[[1]]
+  }, error = function(e) {
+    population_text <- NA
+  })
 
-  area <- driver$findElement(using = "xpath", value = selector_list$area)
-  area_text <- area$getElementText()[[1]]
+  tryCatch({
+    area <- driver$findElement(using = "xpath", value = selector_list$area)
+    area_text <- area$getElementText()[[1]]
+  }, error = function(e) {
+    area_text <- NA
+  })
 
-  age_0_29 <- driver$findElement(using = "xpath", value = selector_list$age_0_29)
-  age_0_29_text <- age_0_29$getElementText()[[1]]
+  tryCatch({
+    age_0_29 <- driver$findElement(using = "xpath", value = selector_list$age_0_29)
+    age_0_29_text <- age_0_29$getElementText()[[1]]
+  }, error = function(e) {
+    age_0_29_text <- NA
+  })
 
-  age_30_64 <- driver$findElement(using = "xpath", value = selector_list$age_30_64)
-  age_30_64_text <- age_30_64$getElementText()[[1]] 
+  tryCatch({
+    age_30_64 <- driver$findElement(using = "xpath", value = selector_list$age_30_64)
+    age_30_64_text <- age_30_64$getElementText()[[1]]
+  }, error = function(e) {
+    age_30_64_text <- NA
+  })
 
-  age_65_plus <- driver$findElement(using = "xpath", value = selector_list$age_65_plus)
-  age_65_plus_text <- age_65_plus$getElementText()[[1]]
+  tryCatch({
+    age_65_plus <- driver$findElement(using = "xpath", value = selector_list$age_65_plus)
+    age_65_plus_text <- age_65_plus$getElementText()[[1]]
+  }, error = function(e) {
+    age_65_plus_text <- NA
+  })
 
-  uc_claimants <- driver$findElement(using = "xpath", value = selector_list$uc_claimants)
-  uc_claimants_text <- uc_claimants$getElementText()[[1]]
+  tryCatch({
+    uc_claimants <- driver$findElement(using = "xpath", value = selector_list$uc_claimants)
+    uc_claimants_text <- uc_claimants$getElementText()[[1]]
+  }, error = function(e) {
+    uc_claimants_text <- NA
+  })
 
-  house_prices <- driver$findElement(using = "xpath", value = selector_list$house_prices)
-  house_prices_text <- house_prices$getElementText()[[1]]
+  tryCatch({
+    house_prices <- driver$findElement(using = "xpath", value = selector_list$house_prices)
+    house_prices_text <- house_prices$getElementText()[[1]]
+  }, error = function(e) {
+    house_prices_text <- NA
+  })
 
-  Sys.sleep(0.3)
+  Sys.sleep(1)
 
   results = list(
     region_nation_text, 
@@ -130,7 +161,8 @@ constituency_dash_scraper <- function(constituency_name){
 }
 
 
-for (i in seq_along(cons$constituency_id)) {
+
+for (i in 13:length(cons$constituency_id)) {
 
   results <- constituency_dash_scraper(cons$cons_name[i])
 
@@ -145,7 +177,16 @@ for (i in seq_along(cons$constituency_id)) {
     cons$age_65_plus_hoclib23[i] <- results[[6]]
 
     cons$uc_claimants_hoclib23[i] <- results[[7]]
-    cons$median_house_price_hoclib23[i] <- results[[8]] 
+    cons$median_house_price_hoclib23[i] <- results[[8]]
+
+ # Cache results collected so far
+  if(i == 1){
+    saveRDS(cons, paste0("data/cache_cons_at", i, ".Rds"))
+  } else {
+     saveRDS(cons, paste0("data/cache_cons_at", i, ".Rds"))
+     file.remove(paste0("data/cache_cons_at", i-1, ".Rds")) # delete last cached object
+  }
+  
 
   Sys.sleep(2)
 
