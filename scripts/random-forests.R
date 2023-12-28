62e819ad2a8756fd5e52a463fd1280e4e79671dd
Len Metson <lennardmetson@gmail.com>
2023-12-28 12:48:39 +0000

    Edit random forest code

--- scripts/random-forests.R
+++ scripts/random-forests.R
@@ -41,20 +41,26 @@ db <- DBI::dbConnect(RSQLite::SQLite(), here("data/parliament_database.sqlite"))
 
 apply_random_forest <- function(
   df, 
-  dv,
+  dv_name = "",
   output_name = "",
   seed = 123, 
   initial_split_prop = 0.8, 
   folds = 5, 
   tuning_levels = 4, 
   cores = parallel::detectCores()) {
+
+    df <- df %>%
+      rename(dv = all_of(dv_name))
+
+    set.seed(seed)
     split <- initial_split(df, prop = initial_split_prop)
 
     train <- training(split)
     test <- testing(split)
     folds <- vfold_cv(train, v = folds)
-
     
+    print(paste0("LOG | ", Sys.time(), " | Splits done." ))
+
     recipe <- recipe(dv ~ ., data = df)
 
     spec <- rand_forest(
@@ -75,15 +81,15 @@ apply_random_forest <- function(
     levels = tuning_levels
     )
 
+  print(paste0("LOG | ", Sys.time(), " | Tuning started." ))
   set.seed(seed)
   tune <- workflow %>%
     tune_grid(
       resamples = folds, 
-      grid = tuning_grid, 
-      control = control_resamples(save_prod = TRUE)
-  )
+      grid = tuning_grid)
 
   saveRDS(tune, paste0("data/random-forest-outputs/", output_name, "_tune.Rds"))
+  print(paste0("LOG | ", Sys.time(), " | Tuning done." ))
   
   final <- workflow %>%
     finalize_workflow(parameters = select_best(tune, "roc_auc"))
@@ -94,6 +100,8 @@ apply_random_forest <- function(
     last_fit(split = split)
   
   saveRDS(last_fit, paste0("data/random-forest-outputs/", output_name, "last_fit.Rds") )
+
+  print(paste0("LOG | ", Sys.time(), " | Final output for '", output_name, "' done. :)" ))
   }
 
 
@@ -128,14 +136,16 @@ analysis_df_econ <- dbGetQuery(
       AND oral_questions.question_tabled_when BETWEEN members.member_date_valid_min AND members.member_date_valid_max
   JOIN constituencies 
     ON members.member_latest_constituency = constituencies.constituency_id
+  
+  LIMIT 2000
   "
 ) %>%
   mutate(is_econ = factor(is_econ)) # TODO: check convert to factor works
 
 
-random_forest(
+apply_random_forest(
   df = analysis_df_econ, 
-  dv = is_econ, 
+  dv_chr = "is_econ", 
   output_name = "econ",
   seed = 1145, 
   initial_split_prop = 0.8,
