e3387434b16ccfa083cb23938f62cec713ba4507
Len Metson <lennardmetson@gmail.com>
2023-12-29 15:57:26 +0000

    Edit functions in setup script and remove progress perc

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -1,3 +1,4 @@
+# Define function to install or load packages
 load_packages <- function(x) {
   y <- x %in% rownames(installed.packages())
   if(any(!y)) install.packages(x[!y])
@@ -30,7 +31,26 @@ load_packages(c(
     "rpart.plot"
     ))
 
+replace_null_with_na <- function(x) {
+  if (is.list(x)) { # Checks for whether the item is a sublist 
+    lapply(x, replace_null_with_na) # if it is, apply the function for each of the elements within the sublist
+  } else { # If it isn't, simply apply the main function
+    ifelse(is.null(x) || x == "null", "NA", x) 
+  }
+}
+
+db_table_check <- function(database, table){
+  rows <- dbGetQuery(database, paste0("SELECT COUNT(1) FROM ", table))
+  cols <- dbListFields(database, table)
+  cols_n <- length(cols)
+  
+  result = list(
+    n_rows = rows[[1]],
+    n_cols = cols_n,
+    col_names = cols)
+  return(result)
+}
+
+
 db <- DBI::dbConnect(RSQLite::SQLite(), here("data/parliament_database.sqlite"))
-
-
 print(paste0("LOG | ", Sys.time(), " | Setup done."))
