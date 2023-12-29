bb133ea7b8e746c54439faa56d5499bbccc318b8
Len Metson <lennardmetson@gmail.com>
2023-12-29 10:24:29 +0000

    Clean up selinium scrape code

--- scripts/04_selinium-scrape-HoC-dashboard.R
+++ scripts/04_selinium-scrape-HoC-dashboard.R
@@ -1,28 +1,13 @@
-# Define function to install or load packages
-load_packages <- function(x) {
-  y <- x %in% rownames(installed.packages())
-  if(any(!y)) install.packages(x[!y])
-  invisible(lapply(x, library, character.only=T))
-  rm(x, y)
-}
+source("scripts/00_setup.R")
 
-# Load required packagess
-load_packages(c(
-    "tidyverse",
-    "here",
-    "DBI",
-    "RSQLite",
-    "httr",
-    "RSelenium"
-    ))
+# Read in data from the constituency endpoint pull
+cons <- readRDS("data/constituencies_raw.Rds") 
 
-
-cons <- readRDS("data/constituencies_raw_basic.Rds")
-
+# Make new dataframe
 cons <- cons %>%
   select(constituency_id, cons_name) %>%
-  unique() %>%
-  mutate(
+  unique() %>% # Keep only unqiue constituencies 
+  mutate( # Initialise variables
     region_nation_hoclib23 = NA,
     population_hoclib23 = NA,
     area_hoclib23 = NA,
@@ -33,15 +18,17 @@ cons <- cons %>%
     median_house_price_hoclib23 = NA
   )
 
+# Set selinium browser
 rD <- rsDriver(browser=c("firefox"), verbose = F, port = netstat::free_port(random = TRUE), chromever = NULL) 
 driver <- rD$client
+
+# Define a list of css selectors
 
 selector_list <- list()
 
 selector_list$search_dropdown <- "/html/body/div[1]/report-embed/div/div[1]/div/div/div/div/exploration-container/div/div/docking-container/div/div/div/div/exploration-host/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[1]/transform/div/div[3]/div/div/visual-modern/div/div/div[2]/div/i"
 
 selector_list$search_box <- "/html/body/div[7]/div[1]/div/div[1]/input"
-
 
 selector_list$search_result <- "/html/body/div[7]/div[1]/div/div[2]/div/div[1]/div/div/div[1]/div/span"
 
@@ -61,29 +48,32 @@ selector_list$uc_claimants <- "/html/body/div[1]/report-embed/div/div[1]/div/div
 
 selector_list$house_prices <- "/html/body/div[1]/report-embed/div/div[1]/div/div/div/div/exploration-container/div/div/docking-container/div/div/div/div/exploration-host/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[39]/transform/div/div[3]/div/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[2]/div[1]"
 
-
-#constituency_name <- cons$cons_name[16]
-#wait_base = 1
-
-constituency_dash_scraper <- function(constituency_name, wait_base = 1){
-  #Sys.sleep(wait_base * 4)
+constituency_dash_scraper <- function(
+  constituency_name, 
+  wait_base = 1 # Allows user to adjust wait lengths (e.g if running on a slow connection)
+                # If you get a 'could not find element' error, try adjusting the wait time as the dashboard takes a while to load 
+  ){
+  # Find dropdown box and click on it 
   search_dropdown <- driver$findElement(using = "xpath", value = selector_list$search_dropdown)
   search_dropdown$clickElement()
-
+  
+  # Find search box and type constituency name
   Sys.sleep(wait_base * 2)
   search_box <- driver$findElement(using = "xpath", value = selector_list$search_box)
-  #search_box$clickElement()
+  #search_box$clickElement() # Do not strictly need this, but if not working try uncommenting
   search_box$clearElement()
   search_box$sendKeysToElement(list(constituency_name))
 
-  Sys.sleep(wait_base * 4)
+  Sys.sleep(wait_base * 4) # This requires a long time to load.
+  # Click on the first result to load data
   first_result <- driver$findElement(using = "xpath", value = selector_list$search_result)
   first_result$clickElement()
-
-  # Extract text 
   
-  # Set defaults as NA
+  Sys.sleep(wait_base * 4) # Wait for data to load
+  
+  # EXTRACT TEXT FROM ELEMENTS
 
+  # Set defaults as NA
   region_nation_text <- NA
   population_text <- NA
   area_text <- NA
@@ -93,18 +83,16 @@ constituency_dash_scraper <- function(constituency_name, wait_base = 1){
   uc_claimants_text <- NA
   house_prices_text <- NA
 
-  Sys.sleep(wait_base * 4)
-
   # Region or nation
-  tryCatch({
+  tryCatch({ # Prevent loop from closing if no data available
   suppressMessages({ 
     region_nation <- driver$findElement(using = "xpath", value = selector_list$region_nation)
     region_nation_text <- region_nation$getElementText()[[1]]
     })
   }, error = function(e) {
+    # Print error message, no need to assign NA as we have set NA as default
     print(paste0("Log: NA assigned for REGION/NATION at iteration ", i))
   })
-
 
   # Population 
   tryCatch({
@@ -165,7 +153,6 @@ constituency_dash_scraper <- function(constituency_name, wait_base = 1){
   })
 
   # House price
-
   tryCatch({
     suppressMessages({
       house_prices <- driver$findElement(using = "xpath", value = selector_list$house_prices)
@@ -175,7 +162,7 @@ constituency_dash_scraper <- function(constituency_name, wait_base = 1){
     print(paste0("Log: NA assigned for HOUSE PRICE at iteration ", i))
   })
 
-
+ # Merge results into a list
   results = list(
     region_nation_text, 
     population_text, area_text, 
@@ -186,18 +173,22 @@ constituency_dash_scraper <- function(constituency_name, wait_base = 1){
 
 }
 
-# Navigate to home page outside of the loop
-driver$deleteAllCookies()
+# Run the scraper
+
+# Navigate to home page outside of the loop to avoid reloading each time
 driver$navigate("https://commonslibrary.parliament.uk/constituency-dashboard/")
 
 Sys.sleep(1)
 
+# The dashboard exists within a sub-page. Unless we "switch" to this subframe, the css paths will be broken
 # Identify and switch to sub-page 
 iframe <- driver$findElement(using = "xpath", value = "//iframe[@title='Constituency dashboard']")
 driver$switchToFrame(iframe)
 Sys.sleep(4)
 
-start_from = 1 # Set the number to start from
+# Set the number to start from in case loop is interuppted but we have cached results
+start_from = 1 
+
 for (i in start_from:length(cons$constituency_id)) {
 
   results <- constituency_dash_scraper(cons$cons_name[i], wait_base = 1)
@@ -223,15 +214,16 @@ for (i in start_from:length(cons$constituency_id)) {
      file.remove(paste0("data/cache_cons_at", i-1, ".Rds")) # delete last cached object
   }
   
-
   Sys.sleep(1)
 
   print(paste0(i, " of ", nrow(cons), " done."))
 
 }
 
+# Kill driver and java processes
 driver$close()
 rD$server$stop()
 system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
 
+# Save output
 saveRDS(cons, "data/hoc_library_scrape_raw.Rds")
